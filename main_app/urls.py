6edfc2ab8154f436ee06ac4b8c41ebe2178b709e
Cody Kremer <ckremer99@outlook.com>
2024-08-31 14:04:58 -0700

    can sign up and sign out users

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('todos/', views.todo_index, name='todo-index'),
     path('todos/<int:todo_id>/', views.todo_detail, name='todo-detail'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- taskflow/settings.py
+++ taskflow/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = 'todo-index'
+
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
